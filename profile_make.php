3353ad7bd2fe4f083159ee8d1916f92f61a31fbc
Gaurav Mittal <gaurav.mittal@students.iiit.ac.in>
2014-04-08 01:06:24 +0530

    Minor bootstrap changes to profile_make.php

--- profile_make.php
+++ profile_make.php
@@ -1,4 +1,17 @@
 <?php
 include("headerfooter.php");
 ?>
-
+<div class="tabbable"> <!-- Only required for left/right tabs -->
+<ul class="nav nav-tabs">
+<li class="active"><a href="#tab1" data-toggle="tab">General Settings</a></li>
+<li><a href="#tab2" data-toggle="tab">Privacy</a></li>
+</ul>
+<div class="tab-content">
+<div class="tab-pane active" id="tab1">
+<!--Enter Code for tab1-->
+</div>
+<div class="tab-pane" id="tab2">
+<!--Enter Code for tab2 -->
+</div>
+</div>
+</div>

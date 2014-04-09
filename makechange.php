5d1a58afe512c3fc825b803068afd4f3a939493a
Gaurav Mittal <gaurav.mittal@students.iiit.ac.in>
2014-04-09 16:58:27 +0530

    Makechange.php completed and half of profile_change.php

--- makechange.php
+++ makechange.php
@@ -1,10 +1,33 @@
 <?php
+include('config.php');
 
+session_start();
+
+$tbl_name="registered_members";
+
+//Store the Post variables in temporary variables.
 $handle=$_POST['handle'];
 $email=$_POST['email'];
 $name=$_POST['username'];
 $dob=$_POST['dob'];
 
-echo ("name= $name email = $email handle = $handle dob=$dob");
+//Store session variable in a variable and renew the session variable.
+$prevhandle=$_SESSION['handle'];
+$_SESSION['handle']=$handle;
 
+//Update table
+$sql="UPDATE $tbl_name 
+SET name='$name', email='$email', handle='$handle', dob='$dob'
+WHERE handle='$prevhandle';";
+$result=mysqli_query($conn,$sql);
+
+//If sql works and its updated.
+if($result){
+//Redirect to profile_make.php page.
+header("Location: profile_make.php");
+}
+//if it doesnt work.
+else{
+echo("Update error.");
+}
 ?>

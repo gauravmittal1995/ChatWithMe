b81f4a01ce994df868282c3ca015e675f8546672
AadityaNair <aadityanair6494@gmail.com>
2014-04-09 22:47:53 +0530

    Setup.php: Upadated it so that it creates a new Database and not work on the default one.

--- setup.php
+++ setup.php
@@ -1,12 +1,12 @@
 <?php
 
 $host="localhost"; // Host name 
-$username="gaurav"; // Mysql username 
-$password=""; // Mysql password 
-$db_name="test"; // Database name 1
+$username="root"; // Mysql username 
+$password="toor"; // Mysql password 
+$db_name="ChatWithMe"; // Database name 1
 
 //Connect to server and select database.
-$conn = mysqli_connect("$host", "$username", "$password","$db_name");
+$conn = mysqli_connect("$host", "$username", "$password",$db_name);
 
 //If connection fails.
 if(! $conn )

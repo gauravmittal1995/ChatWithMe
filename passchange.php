322bf1d1b4fc49681281341730fc2d969a82f9ec
Gaurav Mittal <gaurav.mittal@students.iiit.ac.in>
2014-04-11 03:12:59 +0530

    passchange.php completed (PASSWORD CHANGE ACTIVATED)

--- passchange.php
+++ passchange.php
@@ -1,15 +1,34 @@
 <?php
 
-include("varfromtable");
+include("varfromtable.php");
 
 $currpass = $_POST['currpass'];
 $newpass = $_POST['newpass'];
 $confpass = $_POST['confpass'];
 
 if($password==$currpass){
+
+if($newpass==$confpass){
+
+$tbl_name="registered_members";
+$sql="UPDATE $tbl_name 
+SET password='$newpass'
+WHERE handle='$handle';";
+$result=mysqli_query($conn,$sql);
+
+if($result){
+header("Location: profile_make.php?msg1=PASSWORD CHANGED");
 }
 else{
-	
+header("Location: profile_make.php?msg1=SQL PROBLEM");
+}
+}
+else{
+header("Location: profile_make.php?msg1=NEW PASSWORDS DO NOT MATCH");
+}
+}
+else{
+header("Location: profile_make.php?msg1=INVALID CURRENT PASSWORD");
 }
 
 ?>

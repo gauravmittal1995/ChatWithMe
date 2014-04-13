8ee1d27e1a36de6d18dbdfe9c736c9198e1de3a8
AadityaNair <aadityanair6494@gmail.com>
2014-04-13 21:24:57 +0530

    msg_fetch.php: Updated and Completed.

--- html/msg_fetch.php
+++ html/msg_fetch.php
@@ -1,6 +1,7 @@
 <?php
 
 #  Code to fetch the the last message sent between a Sender and a Reciever
+#  Result is echoed for AJAX Response.
 
 $sender='Nair';
 $reciever="Tulsyan";
@@ -12,15 +13,14 @@ $db_name="ChatWithMe";
 
 $conn = mysqli_connect("$host", "$username", "$password","$db_name");
 if(! $conn)
-{
-	echo "Not Connected\n";
-}
+{	echo "Not Connected\n";}
 
-$fetch="SELECT Message FROM Messages WHERE Sender='$sender' AND Reciever='$reciever' ORDER BY id DESC";
-$test=mysqli_query($conn,$fetch);
-if(! $test)
-	echo "Not Done";
+$fetch="SELECT * FROM Messages WHERE Sender='$sender' AND Reciever='$reciever' ORDER BY id DESC LIMIT 1";
+$res=mysqli_query($conn,$fetch);
+if(! $res)
+{	echo "Not Done";}
 
-
+$row=mysqli_fetch_assoc($res);
+echo $row['Message'];
 
 ?>

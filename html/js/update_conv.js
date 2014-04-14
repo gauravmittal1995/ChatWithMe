e8d4f8d76429079054af63439d8421134c84a3bc
AadityaNair <aadityanair6494@gmail.com>
2014-04-14 20:13:00 +0530

    msg_insert.php : Updated to accept data using GET and POST update_conv.js : Code to Update Database on clicking "Send"

---
+++ html/js/update_conv.js
@@ -0,0 +1,23 @@
+// Code to update the conversation both during "Send" click and regular updates
+
+
+/* 
+ * The sender and reciever is assumed to be provided somewhere in the code.
+ */
+
+function Update_to_DB()
+{
+	function update(e)
+	{
+		
+	}
+
+	
+	
+	sent_msg = document.getElementById("msgtext").innerHTML;
+
+
+
+}
+
+
